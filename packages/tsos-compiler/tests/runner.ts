972b69eda1d908f1caf8085663aa06868f14a555
Brad Jones <brad@bjc.id.au>
2017-10-21 15:04:23 +1100

    chore(solution): latest test setup

--- packages/tsos-compiler/tests/runner.ts
+++ packages/tsos-compiler/tests/runner.ts
@@ -1,17 +1,38 @@
+import { fs } from 'mz';
+import * as shell from 'shelljs';
+import tapSpec = require('tap-spec');
+import tapXUnit = require('tap-xunit');
 import { TestSet, TestRunner } from "alsatian";
 
 (async () =>
 {
-    const testSet = TestSet.create();
+    // Setup the alsatian test runner
+    let testRunner = new TestRunner();
+    let tapStream = testRunner.outputStream;
+    let testSet = TestSet.create();
     testSet.addTestsFromFiles('./**/*.spec.ts');
 
-    const testRunner = new TestRunner();
+    // Setup tap-xunit, this is what provides an
+    // XML XUnit file that CircleCI consumes.
+    let tapToXUnitConverter = tapXUnit();
+    let reportDir = `${__dirname}/../../../test-results/tsos-compiler`;
+    shell.mkdir('-p', reportDir);
+    tapStream.pipe(tapToXUnitConverter).pipe(fs.createWriteStream(`${reportDir}/report.xml`));
+    process.on('exit', (code) =>
+    {
+        if (code === 0 && tapToXUnitConverter.exitCode !== 0)
+        {
+            process.exit(tapToXUnitConverter.exitCode);
+        }
+    });
 
-    testRunner.outputStream
-        .pipe(process.stdout);
+    // This will output a human readable report to the console.
+    tapStream.pipe(tapSpec()).pipe(process.stdout);
 
+    // Runs the tests
     await testRunner.run(testSet);
-})().catch(e =>
+})()
+.catch(e =>
 {
     console.error(e);
     process.exit(1);
