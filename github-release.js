423460f702c1ecbc4425b53d56e5a7e85f4656ac
Brad Jones <brad@bjc.id.au>
2017-11-19 17:07:54 +1100

    feat(solution): automated github release
    This will create a new github release, with the generated changelog from lerna, it will also download the packages just published to npm and attach them to the github release.

---
+++ github-release.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const shell = require('shelljs');
+const lodash = require('lodash');
+const Repository = require('lerna/lib/Repository');
+const PackageUtilities = require('lerna/lib/PackageUtilities');
+
+let gitUrl = lodash.trim(shell.exec('git config --get remote.origin.url', { silent: true }));
+console.log(`Git URL: ${gitUrl}`);
+
+let owner = gitUrl.substring(gitUrl.lastIndexOf(':')+1, gitUrl.lastIndexOf('/'));
+console.log(`Owner: ${owner}`);
+
+let repo = gitUrl.substring(gitUrl.lastIndexOf('/')+1, gitUrl.lastIndexOf('.'));
+console.log(`Repo: ${repo}`);
+
+let latestTag = lodash.trim
+(
+    shell.exec
+    (
+        'git describe --tags --abbrev=0',
+        { silent: true }
+    )
+);
+console.log(`Latest Git Tag: ${latestTag}`);
+
+let secondLastTag = lodash.trim
+(
+    shell.exec
+    (
+        'git describe --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`',
+        { silent: true }
+    )
+);
+console.log(`Second Last Git Tag: ${secondLastTag}`);
+
+let latestTagAnchor = `<a name="${latestTag.replace('v', '')}"></a>`;
+let secondLastTagAnchor = `<a name="${secondLastTag.replace('v', '')}"></a>`;
+
+let changeLog = fs.readFileSync('CHANGELOG.md', 'utf8');
+
+let latestChangeLog = lodash.trim
+(
+    changeLog.substring
+    (
+        changeLog.lastIndexOf(latestTagAnchor) + latestTagAnchor.length,
+        changeLog.lastIndexOf(secondLastTagAnchor)
+    )
+).replace(/#+ \[.*?\](\(.*?\)) \(.*?\)/, '[Click here for the full diff]$1');
+console.log(`\nLatest Changelog:\n---\n${latestChangeLog}\n---\n`);
+
+let uploads = [];
+for (let package of getPackages())
+{
+    let pkg = `@${owner}/${package}@${latestTag.replace('v', '')}`;
+    console.log(`Downloading ${pkg}`);
+    uploads.push(lodash.trim(shell.exec(`npm pack ${pkg}`, { silent:true })));
+}
+
+let gitHubReleaseCmd = `\
+    node ./node_modules/.bin/github-release upload \
+    --owner ${owner} \
+    --repo ${repo} \
+    --tag ${latestTag} \
+    --name ${latestTag} \
+    --body "${latestChangeLog}" \
+    ${uploads.join(' ')} \
+`;
+
+shell.exec(gitHubReleaseCmd);
+
+function getPackages()
+{
+    let repo = new Repository(__dirname);
+
+    let packages = PackageUtilities.getPackages
+    ({
+        rootPath: __dirname, packageConfigs: repo.packageConfigs
+    });
+
+    packages = packages.map(pkg => pkg.name).map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name));
+
+    return packages;
+}
