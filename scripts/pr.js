71da0380aa0b32a89e2e3e7b34f44b8a7d39c787
Brad Jones <brad@bjc.id.au>
2017-11-20 21:54:41 +1100

    ci(solution): aparently when it's succesful, we get nothing at all

--- scripts/pr.js
+++ scripts/pr.js
@@ -4,7 +4,7 @@ const yargs = require('yargs').argv;
 github.client(process.env['GITHUB_TOKEN']).repo('brad-jones/tsos')
 .pr({"title": yargs['title'], "head": yargs['head'], "base": yargs['base']}, (result) =>
 {
-    if (result.body && result.body.errors)
+    if (result && result.body && result.body.errors)
     {
         console.log(result.body);
         process.exit(1);
