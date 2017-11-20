d6cea4932c0f1afd30e43d569a9b613f890dd3bf
Brad Jones <brad@bjc.id.au>
2017-11-20 21:50:37 +1100

    ci(solution): ensure we don't error out when the pr was submitted

--- scripts/pr.js
+++ scripts/pr.js
@@ -4,9 +4,9 @@ const yargs = require('yargs').argv;
 github.client(process.env['GITHUB_TOKEN']).repo('brad-jones/tsos')
 .pr({"title": yargs['title'], "head": yargs['head'], "base": yargs['base']}, (result) =>
 {
-    console.log(result.body);
-    if (result.body.errors)
+    if (result.body && result.body.errors)
     {
+        console.log(result.body);
         process.exit(1);
     }
 });
