57042557e89a2819175047af4826f03d24359421
Brad Jones <brad@bjc.id.au>
2018-01-24 10:48:37 +1100

    feat(tsos-cli): removed the run / runner cmds, now only have the run cmd
    Decided that having the ability to provide "node" options through the run command was just overkill.
    
    On the rare occassion that one needs to supply some custom node options that can call the tsos-cli entry script themselves.
    
    BREAKING CHANGE: Removed runner cmd.

--- packages/tsos-cli/src/app/Commands/Run.ts
+++ packages/tsos-cli/src/app/Commands/Run.ts
@@ -1,9 +1,13 @@
+import * as path from 'path';
 import * as caporal from 'caporal';
-import { spawn } from 'child_process';
-import { injectable } from 'inversify';
+import { Diagnostic } from 'ts-simple-ast';
+import { inject, injectable } from 'inversify';
 import { ICommand } from 'app/Commands/ICommand';
+import { getErrorSource } from 'source-map-support';
 import { ILogger } from 'app/Infrastructure/Logging/ILogger';
 import { ICaporalConfig } from 'app/Infrastructure/ICaporalConfig';
+import { INodeHook } from 'app/Infrastructure/TypeScript/NodeHook';
+import { IDiagnosticFormatter } from 'app/Infrastructure/TypeScript/DiagnosticFormatter';
 
 export interface ICmdArguments
 {
@@ -13,92 +17,95 @@ export interface ICmdArguments
 export interface ICmdOptions
 {
     noCache: boolean;
-    tsOptions: string | undefined;
-    scriptOptions: string | undefined;
-    nodeOptions: string[] | undefined;
-    astVisitors: string | undefined;
+    tsOptions: string[];
+    scriptOptions: string[];
+    astVisitors: string[];
     noVisitors: boolean;
 }
 
 @injectable()
 export default class Run implements ICommand
 {
+    public constructor
+    (
+        @inject(INodeHook) private nodeHook: INodeHook,
+        @inject(IDiagnosticFormatter) private diagnosticFormatter: IDiagnosticFormatter
+    ){}
+
     public Description: string = "Compiles and Runs TypeScript using TsSimpleAst and Ast Visitor Modules on the fly.";
 
     public OnConfigure(config: ICaporalConfig)
     {
         config.argument('script', 'The TypeScript file to run.', caporal.STRING);
         config.option('--no-cache', 'If set transpilation cache will be disabled.', caporal.BOOLEAN, false);
-        config.option('--ts-options', 'A csv list of arguments / options that are accepted by tsc.', caporal.STRING, undefined, false);
-        config.option('--script-options', 'A csv list of arguments / options that are accepted by the script being run.', caporal.STRING, undefined, false);
-        config.option('--node-options', 'A csv list of arguments / options that are accepted by node.', caporal.LIST, undefined, false);
-        config.option('--ast-visitors', 'A csv list of file globs where we might find some visitor modules.', caporal.STRING, undefined, false);
+        config.option('--ts-options', 'A csv list of arguments / options that are accepted by tsc.', caporal.LIST, [], false);
+        config.option('--script-options', 'A csv list of arguments / options that are accepted by the script being run.', caporal.LIST, [], false);
+        config.option('--ast-visitors', 'A csv list of file globs where we might find some visitor modules.', caporal.LIST, [], false);
         config.option('--no-visitors', 'If set, no auto discovered visitors will be applied.', caporal.BOOLEAN, false);
     }
 
     public async OnExecute(args: ICmdArguments, options: ICmdOptions, logger: ILogger)
     {
-        let procArgs: string[] = [];
+        // Take into account the current working directory for relative paths
+        let normalisedScriptPath = args.script.startsWith('/') ? args.script : path.join(process.cwd(), args.script);
+        let normalisedVisitors = options.astVisitors.map(_ => _.startsWith('/') ? _ : path.join(process.cwd(), _));
 
-        // Add any custom node options to the list of arguments
-        // that will be passed to the child node process.
-        if (options.nodeOptions)
+        // This is what makes typescript files get compiled on the fly.
+        this.nodeHook.Register(options.tsOptions, options.noVisitors || normalisedVisitors, !options.noCache);
+
+        // Add the script options into argv
+        if (options.scriptOptions)
         {
-            for (let nodeOption of options.nodeOptions)
+            process.argv.splice(process.argv.findIndex(_ => _ === '--script-options'), 2);
+
+            for (let scriptOption of options.scriptOptions)
             {
-                let segments = nodeOption.split('=');
+                let segments = scriptOption.split('=');
                 if (segments.length > 1)
                 {
                     if (segments[0].length === 1)
                     {
-                        procArgs.push(`-${segments[0]} ${segments[1]}`);
+                        process.argv.push(`-${segments[0]} ${segments[1]}`);
                     }
                     else
                     {
-                        procArgs.push(`--${segments[0]} ${segments[1]}`);
+                        process.argv.push(`--${segments[0]} ${segments[1]}`);
                     }
                 }
                 else
                 {
-                    if (nodeOption.length === 1)
+                    if (scriptOption.length === 1)
                     {
-                        procArgs.push(`-${nodeOption}`);
+                        process.argv.push(`-${scriptOption}`);
                     }
                     else
                     {
-                        procArgs.push(`--${nodeOption}`);
+                        process.argv.push(`--${scriptOption}`);
                     }
                 }
             }
         }
 
-        // Tell the child node process to execute the "Runner" command
-        procArgs.push(__dirname + '/../../index.js');
-        procArgs.push('runner');
-
-        // Proxy arguments and options through to the runner.
-        procArgs.push(args.script);
-        if (options.noCache) procArgs.push('--no-cache');
-        if (options.noVisitors) procArgs.push('--no-visitors');
-        if (options.tsOptions) { procArgs.push('--ts-options'); procArgs.push(options.tsOptions); }
-        if (options.scriptOptions) { procArgs.push('--script-options'); procArgs.push(options.scriptOptions); }
-        if (options.astVisitors) { procArgs.push('--ast-visitors'); procArgs.push(options.astVisitors); }
-
-        // Spawn the child node process
-        let proc = spawn(process.execPath, procArgs, { stdio: 'inherit' });
-        proc.on('exit', function (code: number, signal: string)
+        // Exceptions get swallowed if we do not wrap the require here.
+        try
         {
-            process.on('exit', function ()
+            require(normalisedScriptPath);
+        }
+        catch (e)
+        {
+            let mappedError = getErrorSource(e);
+            if (mappedError) console.error(mappedError);
+
+            if (Array.isArray(e) && e[0] instanceof Diagnostic)
             {
-                if (signal)
-                {
-                    process.kill(process.pid, signal)
-                }
-                else
-                {
-                    process.exit(code)
-                }
-            });
-        });
+                console.error(this.diagnosticFormatter.FormatDiagnostics(e));
+            }
+            else
+            {
+                console.error(e);
+            }
+
+            process.exit(1);
+        }
     }
 }
